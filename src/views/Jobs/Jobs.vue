97f551d5db014429ff2bf94ecf97bce06281fe5b
Rodrigo Lins <rlinsdev@gmail.com>
2021-07-18 10:25:43 -0300

    Dynamic link one

--- src/views/Jobs/Jobs.vue
+++ src/views/Jobs/Jobs.vue
@@ -1,19 +1,24 @@
 <template>
   <h1>Jobs</h1>
   <div v-for="job in jobs" :key="job.id">
-    <h2>{{job.title}}</h2>
+    <h2 @click="jobDetail(job.id)" >{{job.title}}</h2>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
       jobs:[
         {title: 'Ninja UX Designer', id:'1', details: 'Lorem'},
         {title: 'Ninja Web Developer', id:'2', details: 'Lorem'},
         {title: 'Ninja Vue Developer', id:'3', details: 'Lorem'}
       ]
+    }
+  },
+  methods:{
+    jobDetail(id){
+      this.$router.push({ name: 'JobsDetails', params: { id: id } })
     }
   }
 }
