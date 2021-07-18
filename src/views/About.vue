81aaceb20905dbeb10c0fcb5360bb661f2263328
Rodrigo Lins <rlinsdev@gmail.com>
2021-07-18 08:40:43 -0300

    Router links

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos facilis fugiat veritatis tenetur provident velit excepturi consequatur ea reprehenderit et inventore deleniti minima obcaecati maiores, rem esse? Quas, corrupti recusandae!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos facilis fugiat veritatis tenetur provident velit excepturi consequatur ea reprehenderit et inventore deleniti minima obcaecati maiores, rem esse? Quas, corrupti recusandae!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos facilis fugiat veritatis tenetur provident velit excepturi consequatur ea reprehenderit et inventore deleniti minima obcaecati maiores, rem esse? Quas, corrupti recusandae!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos facilis fugiat veritatis tenetur provident velit excepturi consequatur ea reprehenderit et inventore deleniti minima obcaecati maiores, rem esse? Quas, corrupti recusandae!</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Home Page</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos reiciendis natus consectetur ratione! Impedit ullam eum itaque praesentium adipisci repudiandae, nemo modi alias, beatae quisquam tempore sint eius dolorem doloremque ab eaque aspernatur laudantium ea assumenda nisi quaerat totam repellat? Fuga, modi maxime. Perferendis animi nulla aperiam doloribus odio quam nemo, vel libero officiis perspiciatis quos dolore nesciunt iusto blanditiis corrupti est repellat aspernatur veniam eos. A aperiam laboriosam officia corrupti iure qui inventore harum commodi, et sapiente molestiae nisi eum rem optio aspernatur suscipit ex sint accusantium quis fugiat totam ipsum ad deserunt? Ipsum recusandae quaerat sapiente optio in.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos reiciendis natus consectetur ratione! Impedit ullam eum itaque praesentium adipisci repudiandae, nemo modi alias, beatae quisquam tempore sint eius dolorem doloremque ab eaque aspernatur laudantium ea assumenda nisi quaerat totam repellat? Fuga, modi maxime. Perferendis animi nulla aperiam doloribus odio quam nemo, vel libero officiis perspiciatis quos dolore nesciunt iusto blanditiis corrupti est repellat aspernatur veniam eos. A aperiam laboriosam officia corrupti iure qui inventore harum commodi, et sapiente molestiae nisi eum rem optio aspernatur suscipit ex sint accusantium quis fugiat totam ipsum ad deserunt? Ipsum recusandae quaerat sapiente optio in.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos reiciendis natus consectetur ratione! Impedit ullam eum itaque praesentium adipisci repudiandae, nemo modi alias, beatae quisquam tempore sint eius dolorem doloremque ab eaque aspernatur laudantium ea assumenda nisi quaerat totam repellat? Fuga, modi maxime. Perferendis animi nulla aperiam doloribus odio quam nemo, vel libero officiis perspiciatis quos dolore nesciunt iusto blanditiis corrupti est repellat aspernatur veniam eos. A aperiam laboriosam officia corrupti iure qui inventore harum commodi, et sapiente molestiae nisi eum rem optio aspernatur suscipit ex sint accusantium quis fugiat totam ipsum ad deserunt? Ipsum recusandae quaerat sapiente optio in.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { }
 }
 </script>
